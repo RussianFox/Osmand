7f79cdb519fe801bd940fbe7f78acd3298eacf4c
Alexander Chertov <alexander.chertov@etnatrader.com>
2019-09-10 14:04:08 +0300

    Added scripts for Windows

---
+++ OsmAnd/copy_widget_icons.ps1
@@ -0,0 +1,6 @@
+Copy-Item -Force -Path res/drawable-hdpi/widget_* -Destination res/drawable-large/
+Copy-Item -Force -Path res/drawable-xxhdpi/widget_* -Destination res/drawable-large-hdpi/
+Copy-Item -Force -Path res/drawable-xhdpi/widget_* -Destination res/drawable-large-xhdpi/
+Copy-Item -Force -Path res/drawable-hdpi/map_* -Destination res/drawable-large/
+Copy-Item -Force -Path res/drawable-xhdpi/map_* -Destination res/drawable-large-hdpi/
+Copy-Item -Force -Path res/drawable-xxhdpi/map_* -Destination res/drawable-large-xhdpi/
